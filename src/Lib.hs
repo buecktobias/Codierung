8843d9d1520518e3b117ca4b4e8273e02a45f71b
tobias <buecktobias@gmail.com>
2020-06-10 20:39:30 +0200

    Text compression works !!!!

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,12 @@ module Lib where
 
 data BinaryTree = BinaryTree {leftSubTree::Maybe BinaryTree, rightSubTree:: Maybe BinaryTree} | Leaf Char Float deriving (Show, Eq)
 
+data Bit = Bit {v::Int}  deriving Show
+
+getValuesBits:: [Bit] -> [Int]
+getValuesBits bs = map v bs
+
+-- true links    false rechts
 mergeTrees:: BinaryTree -> BinaryTree -> BinaryTree
 mergeTrees b1 b2 = BinaryTree (Just b1) (Just b2)
 
@@ -16,6 +22,10 @@ depthTree Nothing =  0
 depthTree (Just (Leaf _ _)) = 0
 depthTree (Just (BinaryTree left right)) = 1 + (max (depthTree left) (depthTree right))
 
+getChars:: Maybe BinaryTree -> String
+getChars Nothing = []
+getChars (Just (Leaf c _ )) = [c]
+getChars (Just (BinaryTree l r)) = (getChars l) ++ (getChars r)
 
 getTreeLowestFreq:: BinaryTree -> BinaryTree -> BinaryTree
 getTreeLowestFreq b1 b2 = if frequencyTree (Just b1) < frequencyTree (Just b2) then b1 else b2
@@ -47,13 +57,33 @@ getTwoNextTrees bs = ((first, second), bs3)
   (second, bs3) = getNextTree bs2
 
 
+alphabet:: String
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+compressTreeForText:: String -> BinaryTree
+compressTreeForText text = buildCompressingTree startTrees
+  where
+  frequency_alphabet = frequency text alphabet
+  rFreq = relativeFrequency frequency_alphabet
+  startTrees = getStartTrees rFreq
+
 buildCompressingTree:: [BinaryTree] -> BinaryTree
 buildCompressingTree [b] = b
 buildCompressingTree bs = buildCompressingTree (bs2 ++ [newTree])
   where
   ((first, second), bs2) = getTwoNextTrees bs
   newTree = mergeTrees first second
-    
+
+compressChar:: Maybe BinaryTree -> Char -> [Bit]
+compressChar Nothing _ = []
+compressChar (Just (Leaf leafChar _ )) c = []
+compressChar (Just (BinaryTree l r)) c = if (elem c charL) then [Bit 0] ++ compressChar l c else [Bit 1] ++ compressChar r c
+  where
+  charL = getChars l 
+  charR = getChars r
+
+compressText:: Maybe BinaryTree -> String -> [Bit]
+compressText b ss = concat (map (\x -> compressChar b x) ss)
 
 getStartTrees:: [(Char, Float)] -> [BinaryTree]
 getStartTrees freqs = map (\tuple -> Leaf (fst tuple) (snd tuple)) freqs
